4ddeda2ee694ddb883e3a0879dcc87b3becd107f
Rakesh Hemwani <rakeshhemwani654@gmail.com>
2024-12-11 23:12:44 +0530

    packages

---
+++ src/test/java/com/whitewolf/rakesh/week2/SpringMVC/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whitewolf.rakesh.week2.SpringMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
