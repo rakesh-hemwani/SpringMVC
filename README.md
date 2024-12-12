162eb81b2448066bae0c0276e2d567ba421c19c0
Rakesh Hemwani <rakeshhemwani654@gmail.com>
2024-12-12 21:36:32 +0530

    Persistence Layer

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 This is Project is for undersating the MVC structure for web applicatuon 
+
+--For Application.properties
+spring.application.name=SpringMVC
+spring.datasource.url=
+spring.h2.console.enable=true
+spring.datasource.username=
+spring.datasource.password=
+spring.jpa.hibernate.ddl-auto=create
--- src/main/java/com/whitewolf/rakesh/week2/SpringMVC/controllers/EmployeeController.java
+++ src/main/java/com/whitewolf/rakesh/week2/SpringMVC/controllers/EmployeeController.java
@@ -1,13 +1,22 @@
 package com.whitewolf.rakesh.week2.SpringMVC.controllers;
 
 import com.whitewolf.rakesh.week2.SpringMVC.dto.EmployeeDTO;
+import com.whitewolf.rakesh.week2.SpringMVC.entities.EmployeeEntity;
+import com.whitewolf.rakesh.week2.SpringMVC.repositories.EmployeeRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     @GetMapping(path = "/home")
     public String getHomeMessage(){
@@ -15,22 +24,28 @@ public class EmployeeController {
     }
 
     @GetMapping(path = "/{employeeId}")
-    public EmployeeDTO getEmployeeById(@PathVariable(name = "employeeId") Long id){
-        return new EmployeeDTO(id, "Rakesh", "Rakes.Work", 27, LocalDate.of(2024,11,12),true);
+    public EmployeeEntity getEmployeeById(@PathVariable(name = "employeeId") Long id){
+        //new EmployeeDTO(id, "Rakesh", "Rakes.Work", 27, LocalDate.of(2024,11,12),true);
+        return employeeRepository.findById(id).orElse(null);
     }
 
-    @GetMapping(path = "/")
-    public String getEmployeeDetails(@RequestParam(required = false) Integer age){
-        if(age != null)
-            return "Hi Employee Age is" + age;
-        return "You have not passed the age";
+//    @GetMapping(path = "/")
+//    public String getEmployeeDetails(@RequestParam(required = false) Integer age){
+//        if(age != null)
+//            return "Hi Employee Age is" + age;
+//        return "You have not passed the age";
+//    }
+
+    @GetMapping(path = "/all")
+    public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false) Integer age){
+        return employeeRepository.findAll();
     }
 
     @PostMapping
-    public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO employeePayload){
-        employeePayload.setActive(true);
-        employeePayload.setId(100L);
-        return employeePayload;
+    public EmployeeEntity createNewEmployee(@RequestBody EmployeeEntity employeePayload){
+//        employeePayload.setActive(true);
+//        employeePayload.setId(100L);
+        return employeeRepository.save(employeePayload);
     }
 
     @PutMapping String updateEmployeeID(){
